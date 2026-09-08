e3c2c07643f44172bf877d6215dd0c3ac44190ba
Alessio Severi <130257545+alessio-severi@users.noreply.github.com>
2026-09-08 15:43:02 +0200

    Python Modules

--- Module_06/module-06/ft_kaboom_0.py
+++ Module_06/module-06/ft_kaboom_0.py
@@ -4,5 +4,5 @@ import alchemy.grimoire
 if __name__ == "__main__":
     print("=== Kaboom 0 ===")
     print("Testing record light spell:")
-    print(f"{alchemy.grimoire.light_spell_record('Fantasy',
-                                                 'Earth, wind and fire')}")
+    print(alchemy.grimoire.light_spell_record('Fantasy',
+                                              'Earth, wind and fire'))
